8e780ee8dbad4c7932f5f621b2a33d73c38c9509
Changho Youn <younch8342@yu.ac.kr>
2023-10-30 21:54:05 +0900

    solve greedy problem(완전 탐색 시간 over, 백트래킹 too)

--- changddao/src/MakeBigNumber.java
+++ changddao/src/MakeBigNumber.java
@@ -14,15 +14,22 @@ public class MakeBigNumber {
         System.out.println("숫자 number와 제거할 수의 개수를 입력하시오.(입력 예시: 1924 2)");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         number = st.nextToken();
         k = Integer.parseInt(st.nextToken());
         int index = 0; //처음에 가장 큰 수를 담을 변수.
-
-
-
-
+        for (int i = 0; i < number.length() - k; i++) {
+            char selectedMaxNum = '0';
+            for (int j = index; j <= i + k; j++) {
+                if (selectedMaxNum < number.charAt(j)) {
+                    selectedMaxNum = number.charAt(j);
+                    index = j + 1;
+                }
+            }
+            sb.append(selectedMaxNum);
+        }
+        System.out.println(sb.toString());
     }
-
 
 
 }
