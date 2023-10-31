3c892532357a3a0e768d3efab8c662f00698d560
Changho Youn <younch8342@yu.ac.kr>
2023-10-31 09:40:51 +0900

    (프로그래머스) 수열과 구간쿼리 Binary Search로 풀 수 있지만 범위가 작아서 정렬로 품

---
+++ changddao/src/수열과_구간쿼리_2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 수열과_구간쿼리_2 {
+    static int[] arr = {0, 1, 2, 4, 3};
+    static int [][]queries = new int[1000][3];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        int index = 0;
+        while (queries[index++][0] != -1) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int target = Integer.parseInt(st.nextToken());
+            //BinarySearch로 풀고싶은 욕구가 생기나... 처음사고대로 풀어보자
+            int answer = 0;
+            int [] copiedArr = Arrays.copyOfRange(arr, start, end+1);
+            Arrays.sort(copiedArr);
+            for (int i = start; i <= end; i++) {
+                if (copiedArr[i] > target) {
+                    answer = copiedArr[i];
+                    break;
+                }
+                else{
+                    answer=-1;
+                }
+            }
+            System.out.println(answer);
+        }
+        }
+
+    }
+
